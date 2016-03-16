d5548f2d34739b6bc66900c6de6690c83bf4ee18
Chris Hartley <chris.hartley@anymouse.org>
2016-03-16 11:23:33 -0400

    put decorator in urls bc cbv

--- bep_bep/urls.py
+++ bep_bep/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from inventory_tracker import views
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
     # Examples:
@@ -8,5 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.PropertyView.as_view(), name='fieldwork')
+    url(r'^$', staff_member_required(views.PropertyView.as_view()), name='fieldwork')
 ]
