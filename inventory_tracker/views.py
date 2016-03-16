f6e7291636ea0ac1458aac188539c93b99b3f211
Chris Hartley <chris.hartley@anymouse.org>
2016-03-16 11:19:50 -0400

    delete pyc, require staff login

--- inventory_tracker/views.py
+++ inventory_tracker/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.admin.views.decorators import staff_member_required
+
 from .models import photo, Property, claim, status
 from .forms import PropertyFieldworkForm
 
+@staff_member_required
 class PropertyView(SuccessMessageMixin, FormView):
     template_name = 'photo_form.html'
     form_class = PropertyFieldworkForm
