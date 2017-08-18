6f18a31328e41727ced2a3745b516bc5bc0f8982
Chris Hartley <chris.hartley@anymouse.org>
2017-08-18 13:34:49 -0400

    add admin contact

--- bep_bep/settings.py
+++ bep_bep/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
+ADMINS = (('Chris Hartley', 'chris.hartley@renewindianapolis.org'),)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
